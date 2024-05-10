fa2cddf33c588c415808bc1da4aa8ce9bb168c81
kmjak <tymkoron1221@icloud.com>
2024-05-10 17:46:24 +0900

    20240510授業終了

--- 20240510/django_app/hello/urls.py
+++ 20240510/django_app/hello/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('<int:id>/<nickname>/', views.index, name='index'),
+    # path('<int:id>/<nickname>/', views.index, name='index'),
+    path('', views.index, name='index'),
+    # path('form', views.form, name='form'),
+    # path('next', views.next, name='next'),
+    # path('test', views.test, name='test'),
 ]
